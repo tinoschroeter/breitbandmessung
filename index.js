ca40115b383761a5e4e20e471df805d58891564d
Tino Schroter <tino.schroeter@gmail.com>
2022-02-23 16:55:18 +0100

    remove string metrics

--- index.js
+++ index.js
@@ -25,24 +25,25 @@ app.get("/metrics", async (req, res) => {
         result.upload.bandwidth
       )}\n` +
       "# HELP speedtest_packetLoss\n# TYPE speedtest_packetLoss gauge\n" +
-      `speedtest_packetLoss ${result.packetLoss}\n` +
-      "# HELP speedtest_isp\n# TYPE speedtest_isp gauge\n" +
-      `speedtest_isp ${result.isp}\n` +
-      "# HELP speedtest_externalIp\n# TYPE speedtest_externalIp gauge\n" +
-      `speedtest_externalIp ${result.interface.externalIp}\n` +
-      "# HELP speedtest_server_name\n# TYPE speedtest_server_name gauge\n" +
-      `speedtest_server_name ${result.server.name}\n` +
-      "# HELP speedtest_server_location\n# TYPE speedtest_server_location gauge\n" +
-      `speedtest_server_location ${result.server.location}\n` +
-      "# HELP speedtest_server_country\n# TYPE speedtest_server_country gauge\n" +
-      `speedtest_server_country ${result.server.country}\n` +
-      "# HELP speedtest_server_host\n# TYPE speedtest_server_host gauge\n" +
-      `speedtest_server_host ${result.server.host}\n`;
+      `speedtest_packetLoss ${result.packetLoss}\n`;
+    //   +
+    //  "# HELP speedtest_isp\n# TYPE speedtest_isp gauge\n" +
+    //  `speedtest_isp ${result.isp}\n` +
+    //  "# HELP speedtest_externalIp\n# TYPE speedtest_externalIp gauge\n" +
+    //  `speedtest_externalIp ${result.interface.externalIp}\n` +
+    //  "# HELP speedtest_server_name\n# TYPE speedtest_server_name gauge\n" +
+    //  `speedtest_server_name ${result.server.name}\n` +
+    //  "# HELP speedtest_server_location\n# TYPE speedtest_server_location gauge\n" +
+    //  `speedtest_server_location ${result.server.location}\n` +
+    //  "# HELP speedtest_server_country\n# TYPE speedtest_server_country gauge\n" +
+    //  `speedtest_server_country ${result.server.country}\n` +
+    //  "# HELP speedtest_server_host\n# TYPE speedtest_server_host gauge\n" +
+    //  `speedtest_server_host ${result.server.host}\n`;
 
     res.set("Content-Type", "text/plain");
     res.send(output);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.set("Content-Type", "text/plain");
     res.status(500).send(err);
   }
